a11cf230ff7261a60fc803ffd9b03b559ad836fc
jiangchangyun <jiangchangyun@bytedance.com>
2023-07-06 16:39:32 +0800

    修改配置文件

--- byteplus-sdk-java/src/main/java/com/byteplus/service/BaseServiceImpl.java
+++ byteplus-sdk-java/src/main/java/com/byteplus/service/BaseServiceImpl.java
@@ -157,7 +157,7 @@ public abstract class BaseServiceImpl implements IBaseService {
 
         final Properties properties = new Properties();
         try {
-            properties.load(this.getClass().getClassLoader().getResourceAsStream("com/byteplus/version"));
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("version"));
             this.VERSION = properties.getProperty("version");
         } catch (IOException e) {
             LOG.error("Read file version file fail.");

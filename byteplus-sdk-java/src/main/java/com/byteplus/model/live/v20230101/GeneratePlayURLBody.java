3901af5a62bca950cf0161fef578b734c1a9782f
liyan.90210 <liyan.90210@bytedance.com>
2025-10-16 10:57:48 +0000

    feat auto update sdk

--- byteplus-sdk-java/src/main/java/com/byteplus/model/live/v20230101/GeneratePlayURLBody.java
+++ byteplus-sdk-java/src/main/java/com/byteplus/model/live/v20230101/GeneratePlayURLBody.java
@@ -10,12 +10,6 @@ import com.alibaba.fastjson.JSON;
 public final class GeneratePlayURLBody  {
 
     /**
-     * <p>域名空间名称</p>
-     */
-    @com.alibaba.fastjson.annotation.JSONField(name = "Vhost")
-    private String vhost;
-
-    /**
      * <p>拉流域名。您可以调用 [ListDomainDetail](https://www.volcengine.com/docs/6469/1126815) 接口或在视频直播控制台的[域名管理](https://console.volcengine.com/live/main/domain/list)页面，查看直播流使用的拉流域名。</p>
      */
     @com.alibaba.fastjson.annotation.JSONField(name = "Domain")
